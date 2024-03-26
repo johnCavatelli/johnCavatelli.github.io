e10eb4f8bc22f02e7b44ddb785d7f87a51cd52cd
johnCavatelli <ajserck24@gmail.com>
2024-03-26 14:49:58 -0500

    path resolving to art page

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
         korea: resolve(__dirname, 'korea/index.html'),
         hell: resolve(__dirname, 'hell/index.html'),
         phone: resolve(__dirname, 'phone/index.html'),
-        rizz: resolve(__dirname, 'rizz/index.html')
+        rizz: resolve(__dirname, 'rizz/index.html'),
+        art: resolve(__dirname, 'art/index.html')
       },
     },
   },
